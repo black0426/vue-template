153372901b44b23df6c7baf663dd5bc83e92bd6b
v_xbaoxiao <v_xbaoxiao@tencent.com>
2021-06-25 14:28:29 +0800

    局部指令

--- src/views/search/index.vue
+++ src/views/search/index.vue
@@ -1,12 +1,23 @@
 <template>
   <div class="search">
+    <input type="text" v-focus />
     <el-input
+      v-focus
       v-model.trim="searchModel"
       placeholder="搜索详情呀"
       @input="clickInput"
     ></el-input>
+    <!-- input,el-input同时存在的指令v-focus时,input 的自定义指令不会生效 -->
+    <div v-demo:foo.a.b="message"></div>
     <HelloWorld :msg="msg" @test="clickTest"> </HelloWorld>
     <div>{{ testData }}1123</div>
+    <!-- 动态指令参数 -->
+    <div id="baseexample">
+      <p>Scroll down the page(向下滚动页面)</p>
+      <p v-pin:[direction]="350">
+        Stick me 350px from the top of the page(从页面顶部伸出350px)
+      </p>
+    </div>
   </div>
 </template>
 <script>
@@ -20,7 +31,9 @@ export default {
     return {
       searchModel: '',
       msg: 'HelloWorld',
-      testData: ''
+      testData: '',
+      message: 'hello!',
+      direction: 'top'
     }
   },
   created () {
@@ -34,6 +47,18 @@ export default {
     },
     clickInput () {
       console.log('触发clickInput') //在 Input 值改变时触发
+    }
+  },
+  //局部指令
+  directives: {
+    focus: {
+      // 指令的定义
+      inserted: function (el) {
+        if (el.tagName.toLocaleUpperCase() !== 'INPUT') {
+          el = el.getElementsByTagName('input')[0]
+        }
+        el.focus()
+      }
     }
   }
 }
