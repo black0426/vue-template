ddc1b03250c011cb9f80b847f727489e09430be6
v_xbaoxiao <v_xbaoxiao@tencent.com>
2021-07-02 19:56:19 +0800

    Content组件和Home组件

---
+++ src/components/Content.vue
@@ -0,0 +1,104 @@
+<template>
+  <div>
+    <div class="topic" v-for="item in list" :key="item.id">
+      <div @click="goAvatar">
+        <img
+          :src="item.author.avatar_url | imgCheck"
+          @error="imgErr(item.author.avatar_url)"
+          alt="用户头像"
+        />
+      </div>
+      <span class="count">
+        <em>{{ item.reply_count }}</em
+        >/<em>{{ item.visit_count }}</em>
+      </span>
+      <el-tag :type="$tab[item.tab] && $tab[item.tab].type">
+        {{ item.tab ? $tab[item.tab] && $tab[item.tab].name : '无分类' }}
+      </el-tag>
+      <div class="title" @click="goTopic">
+        {{ item.title }}
+      </div>
+      <span class="time">
+        {{
+          $moment(item.last_reply_at, 'YYYY-MM-DD')
+            .startOf('day')
+            .fromNow()
+        }}
+      </span>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: 'Topics',
+  props: ['list'],
+  data () {
+    return {
+      article: []
+    }
+  },
+  created () {},
+  computed: {},
+  methods: {
+    // 图片加载失败方法
+    imgErr (url) {
+      console.log(url)
+    },
+    goAvatar () {
+      this.$router.push({
+        path: '/user/',
+        query: { id: this.author.loginname }
+      })
+    },
+    goTopic () {
+      this.$router.push({
+        path: '/topic/',
+        query: { id: this.id }
+      })
+    }
+  },
+  filters: {
+    imgCheck (url) {
+      /* 源地址： https://avatars[1|2].githubusercontent.com/u/1159760?v=4&s=120
+       * 把avatars后面的数字去掉即可访问到图片
+       */
+      return url.replace(/avatars[0-9]/, 'avatars')
+    }
+  }
+}
+</script>
+<style lang="less" scoped>
+.topic {
+  margin: 10px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  img {
+    width: 50px;
+    border-radius: 50%;
+    height: 50px;
+  }
+  span.count {
+    width: 50px;
+    text-align: center;
+    em:first-child {
+      font-size: 1.2em;
+      color: purple;
+    }
+    em:last-child {
+      color: grey;
+    }
+  }
+  .title {
+    width: 60%;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    white-space: nowrap;
+    margin: 0;
+  }
+  span.time {
+    width: 70px;
+    text-align: right;
+  }
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,27 +1,115 @@
 <template>
   <div class="home">
     <el-tabs v-model="tab" @tab-click="tabChanged">
-      <el-tab-pane label="全部" name="all"></el-tab-pane>
-      <el-tab-pane label="精华" name="good"></el-tab-pane>
-      <el-tab-pane label="分享" name="share"></el-tab-pane>
-      <el-tab-pane label="问答" name="ask"></el-tab-pane>
-      <el-tab-pane label="招聘" name="job"></el-tab-pane>
+      <el-tab-pane label="全部" name="all">
+        <Content :list="list" />
+      </el-tab-pane>
+      <el-tab-pane label="精华" name="good">
+        <Content :list="list" />
+      </el-tab-pane>
+      <el-tab-pane label="分享" name="share">
+        <Content :list="list" />
+      </el-tab-pane>
+      <el-tab-pane label="问答" name="ask">
+        <Content :list="list" />
+      </el-tab-pane>
+      <el-tab-pane label="招聘" name="job">
+        <Content :list="list" />
+      </el-tab-pane>
     </el-tabs>
   </div>
 </template>
 <script>
+import Content from '@/components/Content.vue'
+import { getTopics } from '@/utils/api'
+import { setSession, getSession } from '@/utils/util'
 export default {
   name: 'Home',
+  components: {
+    Content
+  },
   data () {
     return {
-      tab: 'all'
+      page: 1,
+      limit: 20,
+      tab: 'all',
+      list: [],
+      store: {} // 存储所有Tab对应的数据，因为切换Tab后就没必要重新以limit:20加载数据。
     }
   },
-  created () {},
+
+  created () {
+    this.tab = getSession('activeTab') ? getSession('activeTab') : 'all'
+    this.getTopics()
+    window.addEventListener('scroll', this.scrollMethod)
+  },
+  destroyed () {
+    window.addEventListener('scroll', this.scrollMethod)
+  },
   computed: {},
+
   methods: {
-    tabChanged () {}
+    /*
+    当前Tab变化是判断Store里是否已经存储数据
+    是：设置到state中
+    否：重新获取数据
+    */
+    tabChanged () {
+      const store = this.store
+
+      setSession('activeTab', this.tab)
+      // 如果未存储当前Tab的数据，重新获取
+      if (!store[this.tab]) {
+        this.limit = 20
+        this.list = []
+        this.getTopics()
+        return
+      }
+
+      this.list = store[this.tab].data
+      this.limit = store[this.tab].limit
+    },
+
+    /*
+    滚动函数，判断当前滚动条是否到了底部来决定是否重新拉去数据
+    */
+
+    scrollMethod () {
+      const sumH =
+        document.body.scrollHeight || document.documentElement.scrollHeight
+      const viewH = document.documentElement.clientHeight
+      const scrollH =
+        document.body.scrollTop || document.documentElement.scrollTop
+      if (viewH + scrollH >= sumH) {
+        this.getTopics()
+      }
+    },
+
+    getTopics () {
+      getTopics({
+        page: this.page,
+        limit: this.limit,
+        tab: this.tab
+      }).then(res => {
+        this.list = res.data
+        this.limit = this.limit + 10
+
+        const store = this.store
+        store[this.tab] = {
+          limit: this.limit,
+          data: res.data
+        }
+      })
+    }
   }
 }
 </script>
-<style lang="less" scoped></style>
+<style lang="less" scoped>
+.home {
+  margin: auto;
+  width: 75%;
+  padding: 20px 30px;
+  box-sizing: border-box;
+  box-shadow: 0 2px 12px 0 rgba(0, 0, 0, 0.1);
+}
+</style>
