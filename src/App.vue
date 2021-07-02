52ba18d2d6701746063daf50cdcf1c7b7e65f529
v_xbaoxiao <v_xbaoxiao@tencent.com>
2021-07-02 19:55:16 +0800

    更新样式

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <template>
   <div id="app">
     <Header />
-    <router-view></router-view>
+    <div class="main-section">
+      <router-view></router-view>
+    </div>
   </div>
 </template>
 <script>
